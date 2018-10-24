8ab4bfac69f1b33639e65d8fb7862c7df8278837
kerlomz <kerlomz@gmail.com>
2018-10-24 11:24:58 +0800

    feat: Bind a session to a class to extend the simultaneous loading of multiple models.

--- interface.py
+++ interface.py
@@ -15,10 +15,6 @@ from constants import RequestException
 from predict import predict_func
 from utils import ImageUtils
 
-sess = tf.Session()
-init = tf.global_variables_initializer()
-sess.run(init)
-
 
 class Interface(object):
 
@@ -26,25 +22,35 @@ class Interface(object):
         self.model = model
         self.predict = None
         self.x = None
+        self.sess = tf.Session(graph=tf.Graph())
         self.seq_len = None
         self.batch_size = None
-        try:
-            with tf.gfile.GFile(model.compile_model_path, "rb") as f:
-                graph_def = tf.GraphDef()
-                graph_def.ParseFromString(f.read())
-                _ = tf.import_graph_def(graph_def, name="")
-        except NotFoundError:
-            exception('The system cannot find the model specified.')
+        self.graph_def = self.sess.graph.as_graph_def()
         self.load()
 
     def load(self):
-        self.predict = sess.graph.get_tensor_by_name("lstm/output/predict:0")
-        self.x = sess.graph.get_tensor_by_name('input:0')
-        self.seq_len = sess.graph.get_tensor_by_name('lstm/seq_len:0')
-        self.batch_size = sess.graph.get_tensor_by_name('batch_size:0')
-        print('Session Init')
+        try:
+            with tf.gfile.GFile(self.model.compile_model_path, "rb") as f:
+                graph_def_file = f.read()
+            self.graph_def.ParseFromString(graph_def_file)
+        except NotFoundError:
+            exception('The system cannot find the model specified.')
 
-    def predict_b64(self, base64_img, split_char=None):
+        with self.sess.graph.as_default():
+            self.sess.run(tf.global_variables_initializer())
+            _ = tf.import_graph_def(self.graph_def, name="")
+
+        self.predict = self.sess.graph.get_tensor_by_name("lstm/output/predict:0")
+        self.x = self.sess.graph.get_tensor_by_name('input:0')
+        self.seq_len = self.sess.graph.get_tensor_by_name('lstm/seq_len:0')
+        self.batch_size = self.sess.graph.get_tensor_by_name('batch_size:0')
+        print('Session Name: {} Inited.'.format(self.model.target_model))
+
+    def close_session(self):
+        self.sess.close()
+        del self.sess
+
+    def predict_b64(self, base64_img, func, split_char=None):
         e = RequestException()
         # result, code, success = None, 200, True
         try:
@@ -84,5 +90,5 @@ class Interface(object):
             merge_repeated=False,
         )
         dense_decoded = tf.sparse_tensor_to_dense(decoded[0], default_value=-1)
-        predict_text = predict_func(image_batch, sess, dense_decoded, self.batch_size, self.x, self.model, split_char)
+        predict_text = predict_func(image_batch, self.sess, dense_decoded, self.batch_size, self.x, self.model, split_char)
         return predict_text
